caef555650745dd20fb43e6493a807d750f01301
Jose Alexis Hernandez Ricardo <jahernandezricardo@gmail.com>
2022-09-21 03:28:34 -0300

    Completed tests update

--- src/Test.Application.UnitTests/UnitTest.cs
+++ src/Test.Application.UnitTests/UnitTest.cs
@@ -1,21 +1,66 @@
+using Microsoft.VisualStudio.TestPlatform;
+using Moq;
 using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Test.Application.Commands.Company;
+using Test.Application.Validators.Company;
+using Test.Repository;
 using Xunit;
 
 namespace Test.Application.UnitTests;
 
 public class UnitTest
 {
+    const string TEST_CODE = "testCode";
+
     [Fact]
-    public void CreateCompanyValidatorSucceededWhenCompanyDoesNotExist()
+    public async void CreateCompanyValidatorSucceededWhenCompanyDoesNotExist()
     {
-        //Test CreateCompanyValidator
-        //Assert.True(validationResult.IsSuccessful);
+        var companyRepoMock = GetCompanyRepoMock(false);
+        var unitOfWork = GetUnitOfWorkMock(companyRepoMock.Object).Object;
+
+        var validationResult = await new CreateCompanyValidator(unitOfWork)
+            .Validate(new CreateCompanyCommand("testName", TEST_CODE));
+
+        Assert.True(validationResult.IsSuccessful);
+        CommonAsserts(companyRepoMock);
     }
     
     [Fact]
-    public void CreateCompanyValidatorMustFailWhenCompanyExist()
+    public async Task CreateCompanyValidatorMustFailWhenCompanyExistAsync()
     {
-        //Test CreateCompanyValidator
-        //Assert.False(validationResult.IsSuccessful);
+        var companyRepoMock = GetCompanyRepoMock(true);
+        var unitOfWork = GetUnitOfWorkMock(companyRepoMock.Object).Object;
+
+        var validationResult = await new CreateCompanyValidator(unitOfWork)
+            .Validate(new CreateCompanyCommand("testName", TEST_CODE));
+
+        Assert.False(validationResult.IsSuccessful);
+        Assert.Equal("Already exists", validationResult.Error);
+        CommonAsserts(companyRepoMock);
+    }
+
+    private Mock<IUnitOfWork> GetUnitOfWorkMock(ICompanyRepository companyRepository) {       
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        unitOfWorkMock
+            .Setup(x => x.CompanyRepository)
+            .Returns(companyRepository);
+
+        return unitOfWorkMock;
+    }
+
+    private Mock<ICompanyRepository> GetCompanyRepoMock(bool companyExistMockValue) 
+    {
+        var companyRepositoryMock = new Mock<ICompanyRepository>();
+        companyRepositoryMock
+            .Setup(x => x.CompanyExistsAsync(It.IsAny<string>()))
+            .ReturnsAsync(companyExistMockValue);
+
+        return companyRepositoryMock;
+    }
+
+    private void CommonAsserts(Mock<ICompanyRepository> companyRepoMock) 
+    {
+        companyRepoMock.Verify(x => x.CompanyExistsAsync(TEST_CODE),Times.Once);
     }
 }
